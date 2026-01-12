7c53111576594df6035e5ea224c6c406534e9b89
milaaquindts <miladantas0712@gmail.com>
2026-01-12 00:27:54 +0000

    Primeiro commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st. title("Oceano Vivo")
+st.header("Protegendo a vida marinha hoje para garantir o amanhã", divider=True)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
